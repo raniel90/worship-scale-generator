cc63e515ced31f186132f7f89e26dc0284b5617e
Raniel Silva <raniel90@gmail.com>
2021-02-04 23:00:32 -0300

    fix order Data Column

--- app.py
+++ app.py
@@ -166,4 +166,10 @@ def get_gigs():
 
 df = get_gigs()
 today = datetime.date.today()
+
+df.insert(0, 'DataTemp', df['Data'])
+df = df.drop(columns=['Data']).copy()
+df = df.rename(columns={'DataTemp': 'Data'}).copy()
+
+# df.to_csv(f"Escala de músicos {today.year}.csv", index=False)
 df.to_excel(f"Escala de músicos {today.year}.xls", index=False)
